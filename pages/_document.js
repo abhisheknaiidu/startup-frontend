08267df00d84c9dc8e375e7fe54628f0ac17bbb0
abhisheknaiidu <geekyabhi12@gmail.com>
2021-03-17 05:12:08 +0530

    [chore]: server side stylings ✨

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,19 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  // for getting server side rendered style sheets,
+  // because both the client and server render react stuff's
+  // differently, so it should be managed properly!
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    // console.log(styleTags);
+    return { ...page, styleTags };
+  }
   render() {
     return (
       <Html>
