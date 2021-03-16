99e75a3ba46a44f34ac14be11627dfb358460aef
abhisheknaiidu <geekyabhi12@gmail.com>
2021-03-17 04:40:16 +0530

    [feat]: n-progress added for routing 🔥

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
-import Page from "../components/Page";
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Page from '../components/Page';
+// import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+// listen to events
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
+        <Head />
         <body>
           <Main />
           <NextScript />
